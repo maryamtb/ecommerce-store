e85065b5121715acab816e623f410ead210da534
mimooo <maryamtb.17@gmail.com>
2020-07-11 17:41:52 +0100

    shop page ui update

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -17,13 +17,14 @@ const CollectionItem = ({ item, addItem }) => {
           backgroundImage: `url(${imageUrl})`
         }}
       />
-      <div className='collection-footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
-      </div>
-      <CustomButton onClick={() => addItem(item)} inverted>
+      <CustomButton onClick={() => addItem(item)} >
         Add to cart
       </CustomButton>
+      <div className='collection-footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>${price}</span>
+      </div>
+      
     </div>
   );
 };
